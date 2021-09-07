b79ba487b7e72c262a5fe193bf79c72bb01e3912
Lucas <lcscvlcnt@gmail.com>
2021-09-06 22:35:34 -0300

    first commit

---
+++ src/main/java/tk/cavalks/issfortaleza/IssfortalezaApplication.java
@@ -0,0 +1,13 @@
+package tk.cavalks.issfortaleza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssfortalezaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssfortalezaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tk/cavalks/issfortaleza/IssfortalezaApplicationTests.java
@@ -0,0 +1,13 @@
+package tk.cavalks.issfortaleza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssfortalezaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
