06be2d68bfe96362b355c9a4419831e256c48da0
Lucas <lcscvlcnt@gmail.com>
2021-09-19 21:57:05 -0300

    Adição do todo ao comentário
    Esqueci 😅

--- src/main/java/tk/cavalks/issfortaleza/IssfortalezaApplication.java
+++ src/main/java/tk/cavalks/issfortaleza/IssfortalezaApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-// Salvamento em memória dos arquivos de certificados digitais para re-uso em outros envios
+// TODO Salvamento em memória dos arquivos de certificados digitais para re-uso em outros envios
 public class IssfortalezaApplication {
 
 	public static void main(String[] args) {
